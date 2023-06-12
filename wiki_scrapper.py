81efb2480968af6691b272249896a0c5094c02f1
namlosschamlos <136169609+namlosschamlos@users.noreply.github.com>
2023-06-12 18:58:11 +0200

    GeoJson fehlt noch

--- wiki_scrapper.py
+++ wiki_scrapper.py
@@ -1,6 +1,7 @@
 import requests as req
 import pandas as pd
 from bs4 import BeautifulSoup
+import re
 
 bundeslaender = [
     "Baden-Württemberg",
@@ -47,13 +48,25 @@ def scrapperMain():
 
 
 
+def dmsToDd(string):
+    if isinstance(string,str) and any(chr.isdigit() for chr in string) == True and len(string.split(" ")) > 1:
+        degrees = re.findall('\d+',string)[0]
+        minutes = re.findall('\d+',string)[1]
+        seconds = re.findall('\d+',string)[2]
+
+        dd = float(degrees)+float(minutes)/60+float(seconds)/60
+        return dd
+
+
 def prepareCSVData():
     colNames = ["name","constyear","output","type","location","district","dmscoordinates","stakeholder","comment"]
-    df = pd.read_csv("wkaData.csv",sep="\t", encoding="utf-16",header=0)
-    print(df["Koordinaten"])
+    df = pd.read_csv("wkaData.csv",sep="\t", encoding="utf-16",header=0).drop("Unnamed: 10", axis=1)
+    print(df.columns)
 
-    df["latTemp"] = df["Koordinaten"].str.rsplit(",").str[0]
-    print(df["lat"])
+    df["lat"] = (df["Koordinaten"].str.rsplit(",").str[0]).apply(lambda x: dmsToDd(x))
+    df["long"] = (df["Koordinaten"].str.rsplit(",").str[1]).apply(lambda x: dmsToDd(x))
+    df["Name"] = df["Name"].apply(lambda x: re.sub(r'\[\d+\]', "", x))
+
 
 prepareCSVData()
 
